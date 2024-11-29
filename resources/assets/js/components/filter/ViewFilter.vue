0c73565e180b88bb2ccc3826ef81762273041caf
Deanilim <chambideanira@gmail.com>
2024-11-29 16:26:38 -0400

    fil

--- resources/assets/js/components/filter/ViewFilter.vue
+++ resources/assets/js/components/filter/ViewFilter.vue
@@ -2,36 +2,37 @@
   <div class="wrap">
     <div class="body">
       <!-- Filtros de Categoría, Producto y Proveedor -->
-      <div class="row">
+      <div class="row mb-3">
         <div class="col-md-4">
-          <select class="form-control select2" data-live-serach="true" @change="getProduct()" v-model="category">
-            <option value>Todas las categorías</option>
-            <option v-for="(cat, index) in categorys" :key="cat.id" :value="cat.id">{{ cat.name }}</option>
+          <select class="form-control select2" data-live-search="true" @change="getProducts" v-model="category">
+            <option value="">Todas las categorías</option>
+            <option v-for="cat in categorys" :key="cat.id" :value="cat.id">{{ cat.name }}</option>
           </select>
         </div>
 
         <div class="col-md-4">
-          <select class="form-control select2" data-live-serach="true" @change="getData(1)" v-model="product">
-            <option value>Todos los productos</option>
-            <option v-for="(pd, index) in products" :key="pd.id" :value="pd.id">{{ pd.product_name }}</option>
+          <select class="form-control select2" data-live-search="true" @change="updateData" v-model="product">
+            <option value="">Todos los productos</option>
+            <option v-for="pd in products" :key="pd.id" :value="pd.id">{{ pd.product_name }}</option>
           </select>
         </div>
 
         <div class="col-md-4">
-          <select class="form-control select2" data-live-serach="true" @change="getData(1)" v-model="vendor">
-            <option value>Todos los proveedores</option>
-            <option v-for="(vd, index) in vendors" :key="vd.id" :value="vd.id">{{ vd.name }}</option>
+          <select class="form-control select2" data-live-search="true" @change="updateData" v-model="vendor">
+            <option value="">Todos los proveedores</option>
+            <option v-for="vd in vendors" :key="vd.id" :value="vd.id">{{ vd.name }}</option>
           </select>
         </div>
       </div>
 
-      <!-- Cargando indicador -->
-      <div class="loading" v-if="isLoading">
-        <h2 style="text-align:center">Cargando.......</h2>
+      <!-- Indicador de carga -->
+      <div v-if="isLoading" class="text-center">
+        <h4>Cargando datos...</h4>
+        <div class="spinner-border text-primary" role="status"></div>
       </div>
 
       <!-- Tabla de Resultados Filtrados -->
-      <div class="table-responsive" v-else>
+      <div v-else class="table-responsive">
         <table class="table table-condensed table-hover table-bordered">
           <thead>
             <tr>
@@ -51,32 +52,29 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(value, index) in stocks.data" :key="index">
-              <td>{{ value.category.name }}</td>
-              <td>{{ value.product.product_name }}</td>
-              <td>{{ value.vendor.name }}</td>
-              <td>{{ value.chalan_no }}</td>
-              <td>{{ value.stock_quantity }}</td>
-              <td>{{ value.current_quantity }}</td>
-              <td>{{ value.buying_price }}</td>
-              <td>{{ value.selling_price }}</td>
-              <td>{{ value.user.name }}</td>
-              <td>{{ value.created_at | moment('LL') }}</td>
+            <tr v-for="(stock, index) in stocks.data" :key="index">
+              <td>{{ stock.category.name }}</td>
+              <td>{{ stock.product.product_name }}</td>
+              <td>{{ stock.vendor.name }}</td>
+              <td>{{ stock.chalan_no }}</td>
+              <td>{{ stock.stock_quantity }}</td>
+              <td>{{ stock.current_quantity }}</td>
+              <td>{{ stock.buying_price }}</td>
+              <td>{{ stock.selling_price }}</td>
+              <td>{{ stock.user.name }}</td>
+              <td>{{ stock.created_at | moment('LL') }}</td>
               <td>
-                <button @click="editQty(value.id, value.category_id)" type="button"
-                        class="btn bg-blue btn-circle waves-effect waves-circle waves-float">
+                <button @click="editQty(stock.id)" class="btn bg-blue btn-circle waves-effect waves-circle">
                   <i class="material-icons">add</i>
                 </button>
               </td>
               <td>
-                <button @click="editStock(value.id, value.category_id)" type="button"
-                        class="btn bg-blue btn-circle waves-effect waves-circle waves-float">
+                <button @click="editStock(stock.id)" class="btn bg-blue btn-circle waves-effect waves-circle">
                   <i class="material-icons">edit</i>
                 </button>
               </td>
               <td>
-                <button @click="deleteStock(value.id)" type="button"
-                        class="btn bg-pink btn-circle waves-effect waves-circle waves-float">
+                <button @click="deleteStock(stock.id)" class="btn bg-pink btn-circle waves-effect waves-circle">
                   <i class="material-icons">delete</i>
                 </button>
               </td>
@@ -86,7 +84,7 @@
       </div>
 
       <!-- Paginación -->
-      <pagination :pageData="filters"></pagination>
+      <pagination v-if="stocks.meta" :pageData="stocks" @page-clicked="getData"></pagination>
     </div>
   </div>
 </template>
@@ -95,113 +93,92 @@
 import { EventBus } from "../../vue-asset";
 import mixin from "../../mixin.js";
 import MomentMixin from "../../moment_mixin.js";
-
 import editStock from "./editStock.vue";
 import UpdateQuantity from "./UpdateQuantity.vue";
 import Pagination from "../pagination/pagination.vue";
 
 export default {
   props: ["categorys", "vendors"],
-
   mixins: [mixin, MomentMixin],
-
   components: {
     "edit-stock": editStock,
     "update-qty": UpdateQuantity,
     pagination: Pagination,
   },
-
   data() {
     return {
-      stocks: [],
-      name: "",
-      product: "",
+      stocks: { data: [], meta: {} },
       category: "",
+      product: "",
       vendor: "",
       products: [],
-      isLoading: true,
+      isLoading: false,
     };
   },
-
   created() {
     this.getData();
-
-    EventBus.$on("stock-created", function () {
-      window.history.pushState({}, null, location.pathname);
-      this.getData();
-    });
+    EventBus.$on("stock-updated", this.getData);
   },
-
   methods: {
-    // Obtiene los datos filtrados
     getData(page = 1) {
       this.isLoading = true;
       axios
-        .get(base_url + "stock-list?page=" + page + "&product=" + this.product + "&category=" + this.category + "&vendor=" + this.vendor)
-        .then((response) => {
-          this.stocks = response.data;
+        .get(`${base_url}stock-list`, {
+          params: { page, product: this.product, category: this.category, vendor: this.vendor },
+        })
+        .then(({ data }) => {
+          this.stocks = data;
           this.isLoading = false;
         })
-        .catch((error) => {
-          console.log(error);
+        .catch((err) => {
+          console.error(err);
+          this.isLoading = false;
         });
     },
-
-    // Obtiene los productos según la categoría seleccionada
-    getProduct() {
-      if (this.category === "") {
+    getProducts() {
+      if (!this.category) {
         this.products = [];
         this.product = "";
-      } else {
-        this.products = [];
-        axios
-          .get(base_url + "category/product/" + this.category)
-          .then((response) => {
-            this.products = response.data;
-          });
+        this.getData();
+        return;
       }
-
-      this.getData(1);
+      axios
+        .get(`${base_url}category/product/${this.category}`)
+        .then(({ data }) => {
+          this.products = data;
+        })
+        .catch(console.error);
+      this.getData();
     },
-
-    // Función para editar el stock
-    editStock(id, category_id) {
-      EventBus.$emit("edit-stock", id, category_id);
+    updateData() {
+      this.getData();
     },
-
-    // Función para editar la cantidad
+    editStock(id) {
+      EventBus.$emit("edit-stock", id);
+    },
     editQty(id) {
       EventBus.$emit("edit-qty", id);
     },
-
-    // Función para eliminar stock
     deleteStock(id) {
-      Swal(
-        {
-          title: "¿Estás seguro?",
-          text: "¡No podrás revertir esto!",
-          type: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          cancelButtonText: "Cancelar",
-          confirmButtonText: "¡Sí, eliminar!",
-        },
-        () => { }
-      ).then((result) => {
-        if (result.value) {
-          axios.get(base_url + "stock/delete/" + id).then((res) => {
-            EventBus.$emit("stock-created", 1);
-            this.successALert(res.data);
-          });
+      Swal.fire({
+        title: "¿Estás seguro?",
+        text: "¡No podrás revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, eliminar",
+        cancelButtonText: "Cancelar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios
+            .delete(`${base_url}stock/delete/${id}`)
+            .then(({ data }) => {
+              this.getData();
+              this.$toast.success(data.message || "Stock eliminado correctamente.");
+            })
+            .catch(console.error);
         }
       });
     },
-
-    // Función para paginación
-    pageClicked(pageNo) {
-      this.getData(pageNo);
-    }, 
   },
 };
 </script>
