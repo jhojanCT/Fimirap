e24a25e983a4c8e42ad41e28543d3dfb99b55bb7
Deanilim <chambideanira@gmail.com>
2024-11-30 09:28:02 -0400

    fi

--- resources/views/filter/filter.blade.php
+++ resources/views/filter/filter.blade.php
@@ -1,40 +1,48 @@
 @extends('include.master')
 
-@section('title', 'Inventory | Filtrado')
+@section('title', 'Inventory | Filtros')
 
-@section('page-title', 'Lista de Control de Calidad')
+@section('page-title', 'Lista de Filtrado')
 
 @section('content')
 
-<div class="container-fluid">
-    <!-- Sección de creación de filtrados -->
-    <div class="row clearfix">
+<div class="row clearfix">
+    <!-- Componente para realizar el filtrado -->
+    <create-filter 
+        :date="{{ json_encode(date('Y-m-d')) }}" 
+        :vendors="{{ json_encode($vendors) }}"  <!-- Aseguramos el formato JSON -->
+        :categorys="{{ json_encode($categories) }}">
+    </create-filter>
+</div>
+
+<div class="row clearfix">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="card">
             <div class="header">
                 <h2>
-                    Gestión de Filtros y Atributos
+                    <!-- Botón para agregar existencias -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#create-stock">
+                        Agregar Filtrado
+                    </button>
                 </h2>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#create-filter">
-                    Agregar Filtrado
-                </button>
             </div>
+
             <div class="body">
-                <!-- Aquí se incluye el componente de creación de filtros -->
-                <create-filter 
-                    :date="{{ json_encode(date('Y-m-d')) }}" 
-                    :vendors="{{ $vendors }}" 
-                    :categorys="{{ $categories }}">
-                </create-filter>
+                <!-- Componente para mostrar los resultados del filtro -->
+                <view-filter 
+                    :vendors="{{ json_encode($vendors) }}" 
+                    :categorys="{{ json_encode($categories) }}" 
+                    :products="{{ json_encode($products) }}">
+                </view-filter>
             </div>
         </div>
     </div>
 </div>
 
-</div>
-
 @endsection
 
 @push('script')
+
 <script type="text/javascript" src="{{ url('public/js/filter.js') }}"></script>
+
 @endpush
