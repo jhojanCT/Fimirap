d785fa540878230753b38afa0a9a0a12a36249c9
Deanilim <chambideanira@gmail.com>
2024-12-10 11:16:20 -0400

    jjjjj

---
+++ app/Http/Controllers/ContaController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\Stock;
+use App\Category;
+use Session;
+use Illuminate\Http\Request; 
+
+class ContaController extends Controller
+{
+    public function index()
+    {
+
+        // Pasar los datos a la vista
+        return view('conta.conta');
+    }
+}
